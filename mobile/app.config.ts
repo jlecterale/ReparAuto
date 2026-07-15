1b4d80f9c686dd7c9ccb03eec00adb7576704834
Flavio F Lima <flavioislima@users.noreply.github.com>
2026-07-15 13:05:14 +0100

    feat: update version to 1.9.0 and build number to 90 in app.config.ts

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -56,8 +56,8 @@ if (!GOOGLE_IOS_URL_SCHEME) {
 // dev builds fall back to `development`.
 const APS_ENVIRONMENT =
   process.env.APS_ENVIRONMENT === 'production' ? 'production' : 'development';
-const VERSION = '1.8.2';
-const BUILD_NUMBER = 83;
+const VERSION = '1.9.0';
+const BUILD_NUMBER = 90;
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
